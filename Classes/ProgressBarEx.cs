ae095b1a773a99fbb1e04d1d5dd13dc11ac4452d
KomiksGit <viijam0817@gmail.com>
2022-12-04 17:37:05 +0800

    Added property ProgressBarImage for ProgressBarEx

--- Classes/ProgressBarEx.cs
+++ Classes/ProgressBarEx.cs
@@ -1,42 +1,41 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
-using SRO_Installer_Boobies.Properties;
 
 namespace SRO_Installer_Boobies.Classes
 {
     internal class ProgressBarEx : UserControl
     {
-        private int min;// Minimum value for progress range
-        private int max = 100;// Maximum value for progress range
-        private int val;// Current progress
-        private Color BarColor1 = Color.DarkOrchid;// Color of progress meter
-        private Color BarColor2 = Color.DeepSkyBlue;// Color of progress meter
+        private int val, min, max = 100;
+        private Color BarColor1 = Color.DarkOrchid;
+        private Color BarColor2 = Color.DeepSkyBlue;
+        private Image BarImage;
 
-        protected override void OnResize(EventArgs e)
-        {
-            // Invalidate the control to get a repaint.
-            Invalidate();
-        }
+        protected override void OnResize(EventArgs e) => Invalidate();
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            var g = e.Graphics;
-            //var brush = new LinearGradientBrush(new Point(0, 0), new Point(0, 1), BarColor1, BarColor2);
-            var brush = new TextureBrush(Resources.install_bar);
-            var percent = (val - min) / (float)(max - min);
-            var rect = ClientRectangle;
-            // Calculate area for drawing the progress.
-            rect.Width = (int)(rect.Width * percent);
+            Brush brush;
 
-            // Draw the progress meter.
+            var g = e.Graphics;
+            
+            if (BarImage == null)
+            {
+                brush = new LinearGradientBrush(new Point(0, 0), new Point(0, 1), BarColor1, BarColor2);
+            }
+            else brush = new TextureBrush(BarImage);
+
+            var percent = (val - min) / (float)(max - min);
+
+            var rect = ClientRectangle;
+
+            rect.Width = (int)(rect.Width * percent);
+            
             g.FillRectangle(brush, rect);
 
-            // Draw a three-dimensional border around the control.
-            //Draw3DBorder(g);
-
-            // Clean up.
             brush.Dispose();
+
             g.Dispose();
         }
 
@@ -46,27 +45,23 @@ namespace SRO_Installer_Boobies.Classes
 
             set
             {
-                // Prevent a negative value.
                 if (value < 0)
                 {
                     value = 0;
                 }
-
-                // Make sure that the minimum value is never set higher than the maximum value.
+                
                 if (value > max)
                 {
                     max = value;
                 }
 
                 min = value;
-
-                // Ensure value is still in range
+                
                 if (val < min)
                 {
                     val = min;
                 }
-
-                // Invalidate the control to get a repaint.
+                
                 Invalidate();
             }
         }
@@ -77,21 +72,18 @@ namespace SRO_Installer_Boobies.Classes
 
             set
             {
-                // Make sure that the maximum value is never set lower than the minimum value.
                 if (value < min)
                 {
                     min = value;
                 }
 
                 max = value;
-
-                // Make sure that value is still in range.
+                
                 if (val > max)
                 {
                     val = max;
                 }
-
-                // Invalidate the control to get a repaint.
+                
                 Invalidate();
             }
         }
@@ -103,8 +95,7 @@ namespace SRO_Installer_Boobies.Classes
             set
             {
                 var oldValue = val;
-
-                // Make sure that the value does not stray outside the valid range.
+                
                 if (value < min)
                 {
                     val = min;
@@ -117,23 +108,18 @@ namespace SRO_Installer_Boobies.Classes
                 {
                     val = value;
                 }
-
-                // Invalidate only the changed area.
-
+                
                 var newValueRect = ClientRectangle;
                 var oldValueRect = ClientRectangle;
-
-                // Use a new value to calculate the rectangle for progress.
+                
                 var percent = (val - min) / (float)(max - min);
                 newValueRect.Width = (int)(newValueRect.Width * percent);
-
-                // Use an old value to calculate the rectangle for progress.
+                
                 percent = (oldValue - min) / (float)(max - min);
                 oldValueRect.Width = (int)(oldValueRect.Width * percent);
 
                 var updateRect = new Rectangle();
-
-                // Find only the part of the screen that must be updated.
+                
                 if (newValueRect.Width > oldValueRect.Width)
                 {
                     updateRect.X = oldValueRect.Size.Width;
@@ -146,8 +132,7 @@ namespace SRO_Installer_Boobies.Classes
                 }
 
                 updateRect.Height = Height;
-
-                // Invalidate the intersection region only.
+                
                 Invalidate(updateRect);
             }
         }
@@ -159,8 +144,6 @@ namespace SRO_Installer_Boobies.Classes
             set
             {
                 BarColor1 = value;
-
-                // Invalidate the control to get a repaint.
                 Invalidate();
             }
         }
@@ -172,28 +155,19 @@ namespace SRO_Installer_Boobies.Classes
             set
             {
                 BarColor2 = value;
-
-                // Invalidate the control to get a repaint.
                 Invalidate();
             }
         }
 
-        private void Draw3DBorder(Graphics g)
+        public Image ProgressBarImage
         {
-            var PenWidth = (int)Pens.White.Width;
+            get => BarImage;
 
-            g.DrawLine(Pens.DarkGray,
-            new Point(ClientRectangle.Left, ClientRectangle.Top),
-            new Point(ClientRectangle.Width - PenWidth, ClientRectangle.Top));
-            g.DrawLine(Pens.DarkGray,
-            new Point(ClientRectangle.Left, ClientRectangle.Top),
-            new Point(ClientRectangle.Left, ClientRectangle.Height - PenWidth));
-            g.DrawLine(Pens.White,
-            new Point(ClientRectangle.Left, ClientRectangle.Height - PenWidth),
-            new Point(ClientRectangle.Width - PenWidth, ClientRectangle.Height - PenWidth));
-            g.DrawLine(Pens.White,
-            new Point(ClientRectangle.Width - PenWidth, ClientRectangle.Top),
-            new Point(ClientRectangle.Width - PenWidth, ClientRectangle.Height - PenWidth));
+            set
+            {
+                BarImage = value;
+                Invalidate();
+            }
         }
     }
 }
